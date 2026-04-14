b04be9223d50822ec58a7bf44a2ddf7cf414b69a
LuisOtavioSalesMendes <qetiotee@gmail.com>
2026-04-14 11:39:52 -0300

    dados.php
    adicionando mais dados

--- MEDEVE/sistema/data/dados.php
+++ MEDEVE/sistema/data/dados.php
@@ -8,7 +8,10 @@ $usuarios = [
 
 $contas = [
     ["descricao" => "Emprestimo", "valor" => 2.65, "tipo" => "Receber", "status" => "Pendente"],
-    ["descricao" => "Internet", "valor" => 100, "tipo" => "Pagar", "status" => "Pendente"]
+    ["descricao" => "Internet", "valor" => 100, "tipo" => "Pagar", "status" => "Pendente"],
+    ["descricao" => "Salgado", "valor" => 11, "tipo" => "Pagar", "status" => "Pendente"],
+    ["descricao" => "Pão de queijo", "valor" => 4.50, "tipo" => "Pagar", "status" => "Pendente"],
+    ["descricao" => "Dividendos", "valor" => 12, "tipo" => "Receber", "status" => "Pendente"]
 ];
 
 ?>
